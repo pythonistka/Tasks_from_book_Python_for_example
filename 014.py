35297bd46e540399eb9c4aa216f88923f32eed21
Lopatina Darya <baboleh@rambler.ru>
2022-04-05 01:13:40 +0300

    Добавлена задача 014 (по if)

---
+++ 014.py
@@ -0,0 +1,7 @@
+number_of_user = int(input("Введите число от 10 до 20 включительно: "))
+num_1 = 10
+num_2 = 20
+if num_1 <= number_of_user <= num_2:
+    print("Спасибо")
+else:
+    print("Некорректное значение")
