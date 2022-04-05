3e4c0a8457076c3363f73821796c7580050d9b76
Lopatina Darya <baboleh@rambler.ru>
2022-04-05 15:55:10 +0300

    Добавлена задача 022 (Строки)

---
+++ 022.py
@@ -0,0 +1,6 @@
+name = input('Введите ваше имя строчными буквами: ')
+surname = input('Введите вашу фамилию строчными буквами: ')
+phrise = name + " " + surname
+phrise = phrise.title()
+
+print(phrise)
