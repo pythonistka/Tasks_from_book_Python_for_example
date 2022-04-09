ef6e292939896612fbba9b88b592a1509f99277f
Lopatina Darya <baboleh@rambler.ru>
2022-04-09 23:55:41 +0300

    Добавлена задача 027 (Математические операции)

---
+++ 027.py
@@ -0,0 +1,5 @@
+num = float(input('Введите число с большим количеством знаков в дробной части: '))
+coefficient = int(input("Введите коэффициент для умножения вашего числа: "))
+result = num * coefficient
+
+print(f'Ваше число умноженное на {coefficient}: {result}')
