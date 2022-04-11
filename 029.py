19e6933dbd4d4088907b350dce1991479447b48c
Lopatina Darya <baboleh@rambler.ru>
2022-04-11 23:57:45 +0300

    Добавлена задача 029 (Математические операции)

---
+++ 029.py
@@ -0,0 +1,7 @@
+import math
+
+num = int(input("Введите число больше 500: "))
+square_root = math.sqrt(num)
+square_root = round(square_root, 2)
+
+print(f"Квадратный корень из вашего числа равен: {square_root}")
