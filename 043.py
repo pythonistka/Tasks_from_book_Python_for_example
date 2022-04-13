ea01d151e0373d9c35e988c9cce94030c352f81f
Lopatina Darya <baboleh@rambler.ru>
2022-04-14 01:36:42 +0300

    Добавлена задача 043 (Цикл for)

---
+++ 043.py
@@ -0,0 +1,13 @@
+answer = input("В каком направлении вести отсчёт, в прямом или обратном?(п/о): ")
+answer_1 = "п"
+answer_2 = "о"
+if answer == answer_1:
+    num = int(input("Введите число: "))
+    for i in range(1, num + 1):
+        print(i)
+elif answer == answer_2:
+    num = int(input("Введите число меньше 20: "))
+    for i in range(20, num -1, -1):
+        print(i)
+else:
+    print("Я вас не понимаю")
