0014d8866a1950ae330d386bbdf69dfb58230493
Lopatina Darya <baboleh@rambler.ru>
2022-04-20 01:52:59 +0300

    Добавлена окраска фигуры

--- факультатив.py
+++ факультатив.py
@@ -1,11 +1,14 @@
 import turtle
 
+
 turtle.shape("turtle")
 turtle.pensize(3)
 turtle.color("black", "red")
+turtle.begin_fill()
 for i in range(0, 5):
     turtle.forward(90)
     turtle.right(30)
     turtle.forward(90)
     turtle.left(102)
+turtle.end_fill()
 turtle.exitonClick()
