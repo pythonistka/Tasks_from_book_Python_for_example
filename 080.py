481b07989448059f562995ad05d1ea54ac75ba82
Lopatina Darya <baboleh@rambler.ru>
2022-04-25 00:15:55 +0300

    Добавлена задача 080 (Кортежи, списки, словари)

---
+++ 080.py
@@ -0,0 +1,7 @@
+name = input("Введите ваше имя: ")
+print(f"Длина вашего имени: {len(name)}")
+surname = input("Введите вашеу фамилию: ")
+print(f"Длина вашей фамилии: {len(surname)}")
+all_name = name +" "+ surname
+print(all_name)
+print(f"Длина вашего полного имени: {len(all_name)}")
