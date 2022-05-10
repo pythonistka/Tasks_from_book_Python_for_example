dff216b484364a007302c820aea6c022278e099d
Lopatina Darya <baboleh@rambler.ru>
2022-05-10 23:59:46 +0300

    Добавлена задача 117 (Чтение и запись файлов .csv)

--- 117.py
+++ 117.py
@@ -3,7 +3,7 @@ import csv
 
 file = open("Questions.csv", "a")
 name = input("Ввелите ваше имя: ")
-question_1 = int(input(random.choice(["Сколько вам лет?: ", "Какого вы года рождения?: "])))
+question_1 = input(random.choice(["Сколько вам лет?: ", "Какого вы года рождения?: "]))
 question_2 = input(random.choice(["У вас есть домашнее животное?: ", "Вы замужем?: "]))
 new_record = name + ", " + question_1 + ", " + question_2 + "\n"
 file.write(str(new_record))
