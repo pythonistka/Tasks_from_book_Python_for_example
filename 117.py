b0decfeba51f8635545b83cd54e652584a41a610
Lopatina Darya <baboleh@rambler.ru>
2022-05-10 23:58:54 +0300

    Добавлена задача 117 (Чтение и запись файлов .csv)

--- 117.py
+++ 117.py
@@ -1,11 +1,10 @@
+import random
 import csv
 
-file = list(csv.reader(open("Books.csv")))
-tmp = []
-for row in file:
-    tmp.append(row)
-
-x = 0
-for row in tmp:
-    print(x, row)
-    x = x + 1
+file = open("Questions.csv", "a")
+name = input("Ввелите ваше имя: ")
+question_1 = int(input(random.choice(["Сколько вам лет?: ", "Какого вы года рождения?: "])))
+question_2 = input(random.choice(["У вас есть домашнее животное?: ", "Вы замужем?: "]))
+new_record = name + ", " + question_1 + ", " + question_2 + "\n"
+file.write(str(new_record))
+file.close()
