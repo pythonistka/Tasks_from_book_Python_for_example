78298dcd09c6cce8a662e82392d1ea3563a53281
Lopatina Darya <baboleh@rambler.ru>
2022-05-11 00:23:07 +0300

    Отредактирована задача 117 (Чтение и запись текстовых файлов)

--- 117.py
+++ 117.py
@@ -1,10 +1,28 @@
 import random
 import csv
 
+score = 0
+name = input("Введите ваше имя: ")
+num_1 = random.randint(1, 10)
+num_2 = random.randint(1, 10)
+question_1 = str(num_1) + "+" + str(num_2) + "="
+ans_1 = int(input(question_1))
+real_ans_1 = num_1 + num_2
+
+if ans_1 == real_ans_1:
+    score = score + 1
+
+num_3 = random.randint(1, 10)
+num_4 = random.randint(1, 10)
+question_2 = str(num_3) + "+" + str(num_4) + "="
+ans_2 = int(input(question_2))
+real_ans_2 = num_3 + num_4
+
+if ans_2 == real_ans_2:
+    score = score + 1
+
 file = open("Questions.csv", "a")
-name = input("Ввелите ваше имя: ")
-question_1 = input(random.choice(["Сколько вам лет?: ", "Какого вы года рождения?: "]))
-question_2 = input(random.choice(["У вас есть домашнее животное?: ", "Вы замужем?: "]))
-new_record = name + ", " + question_1 + ", " + question_2 + "\n"
+new_record = name + ", " + question_1 + str(ans_1) + ", " + question_2 + str(ans_2) + ", " + str(score) + "\n"
 file.write(str(new_record))
+
 file.close()
