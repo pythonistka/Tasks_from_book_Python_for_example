1d01e7b71ca4004f35b4a1b72d17d6a49849878c
Lopatina Darya <baboleh@rambler.ru>
2022-05-24 00:24:36 +0300

    Добавлена задача 128 (Графический интерфейс  Tkinter)

--- 128.py
+++ 128.py
@@ -15,7 +15,7 @@ def add_on_km():
 
 window = Tk()
 window.title("Расчет дистанции")
-window.geometry("450x200")
+window.geometry("450x100")
 
 enter_lbl = Label(text="Введите длину: ")
 enter_lbl.place(x=50, y=20, width=100, height=25)
@@ -35,7 +35,7 @@ output_lbl = Label(text="Результат:")
 output_lbl.place(x=50, y=50, width=100, height=25)
 
 output_num = Message(text=0)
-output_num.place(x=150, y=50, width=100, height=100)
+output_num.place(x=150, y=50, width=100, height=25)
 output_num["bg"] = "white"
 output_num["relief"] = "sunken"
 output_num["justify"] = "center"
