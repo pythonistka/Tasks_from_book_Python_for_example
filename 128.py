c59911b58f0a400d1641655dc86b7eafc1db42d0
Lopatina Darya <baboleh@rambler.ru>
2022-05-24 00:19:47 +0300

    Добавлена задача 128 (Графический интерфейс  Tkinter) устранены ошибки

--- 128.py
+++ 128.py
@@ -9,15 +9,15 @@ def add_on_mile():
 def add_on_km():
     num = enter_num.get()
     num = int(num)
-    mile = num * 1.6093
-    output_num["text"] = mile
+    km = num * 1.6093
+    output_num["text"] = km
 
 
 window = Tk()
 window.title("Расчет дистанции")
-window.geometry("200x180")
+window.geometry("450x200")
 
-enter_lbl = Label(text="Введите расстояние: ")
+enter_lbl = Label(text="Введите длину: ")
 enter_lbl.place(x=50, y=20, width=100, height=25)
 
 enter_num = Entry(text=0)
@@ -29,7 +29,7 @@ mile_button = Button(text="Расчет в милях", command=add_on_mile)
 mile_button.place(x=300, y=20, width=120, height=25)
 
 km_button = Button(text="Расчет в км", command=add_on_km)
-km_button.place(x=300, y=20, width=120, height=25)
+km_button.place(x=300, y=50, width=120, height=25)
 
 output_lbl = Label(text="Результат:")
 output_lbl.place(x=50, y=50, width=100, height=25)
