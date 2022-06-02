26ad87c30006f0ca352d61098d06eb902f0b697f
Lopatina Darya <baboleh@rambler.ru>
2022-06-03 02:35:39 +0300

    Добавлена задача 136 (Подробнее о Tkinter)

--- 136.py
+++ 136.py
@@ -1,5 +1,14 @@
 from tkinter import *
 
+def add_to_list():
+    name = enter_name.get()
+    sex = selectSex.get()
+    name_sex = name + ", " + sex
+    show_section.insert(END, name_sex)
+    enter_name.delete(0, END)
+    selectSex.set("Ваш пол")
+
+
 window = Tk()
 window.title("Список имён")
 window.geometry("450x200")
@@ -15,6 +24,18 @@ enter_name.focus()
 selectSex = StringVar(window)
 selectSex.set("Ваш пол")
 colorList = OptionMenu(window, selectSex, "Женский", "Мужской")
-colorList.place(x = 150, y = 60)
+colorList.place(x = 280, y = 17)
+
+add_button = Button(text="Добавить", command=add_to_list)
+add_button.place(x=150, y=60, width=100, height=25)
+
+show_lbl = Label(text="Cписок: ")
+show_lbl.place(x=50, y=95, width=100, height=25)
+
+show_section = Listbox()
+show_section.place(x=150, y=100, width=100, height=70)
+show_section["justify"] = "center"
+show_section.focus()
+
 
 window.mainloop()
