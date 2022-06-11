091c586d50117047e64a113ef9333ba5d2b1acdd
Lopatina Darya <baboleh@rambler.ru>
2022-06-11 19:46:01 +0300

    Создана таблица Books в задаче 141 (SqLite)

--- 141.py
+++ 141.py
@@ -3,27 +3,18 @@ import sqlite3
 with sqlite3.connect("BookInfo.db") as db:
     cursor = db.cursor()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS Authors(
-name text PRIMARY KEY,
-plaseofbirth text NOT NULL);""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS Books(
+id integer PRIMARY KEY,
+title text NOT NULL,
+author text NOT NULL,
+datepublished integer);""")
 
-cursor.execute("""INSERT INTO Authors(name, plaseofbirth)
-VALUES("Agatha Christie", "Torquay")""")
+cursor.execute("""INSERT INTO Books(title, author, datepublished)
+VALUES("De Profundis", "Oscar Wilde", "1905")""")
 db.commit()
 
-cursor.execute("""INSERT INTO Authors(name, plaseofbirth)
-VALUES("Cecelia Ahern", "Dublin")""")
-db.commit()
 
-cursor.execute("""INSERT INTO Authors(name, plaseofbirth)
-VALUES("J. K. Rowling", "Bristol")""")
-db.commit()
-
-cursor.execute("""INSERT INTO Authors(name, plaseofbirth)
-VALUES("Oscar Wilde", "Dublin")""")
-db.commit()
-
-cursor.execute("SELECT * FROM Authors")
+cursor.execute("SELECT * FROM Books")
 for x in cursor.fetchall():
     print(x)
 
