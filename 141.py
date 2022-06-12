f1a6354c24fdbf4fd6dfb74b4fb90f686bb43809
Lopatina Darya <baboleh@rambler.ru>
2022-06-12 11:42:18 +0300

    Добавлена задача 141 (SqLite)

--- 141.py
+++ 141.py
@@ -3,16 +3,47 @@ import sqlite3
 with sqlite3.connect("BookInfo.db") as db:
     cursor = db.cursor()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS Books(
-id integer PRIMARY KEY,
-title text NOT NULL,
-author text NOT NULL,
-datepublished integer);""")
 
 cursor.execute("""INSERT INTO Books(title, author, datepublished)
-VALUES("De Profundis", "Oscar Wilde", "1905")""")
+VALUES("Harry Potter and the prisoner of Azkaban", "J. K. Rowling", "1999")""")
 db.commit()
 
+cursor.execute("""INSERT INTO Books(title, author, datepublished)
+VALUES("Lyrebird", "Cecelia Ahern", "2017")""")
+db.commit()
+
+cursor.execute("""INSERT INTO Books(title, author, datepublished)
+VALUES("Murder on the Orient Express", "Agatha Christie", "1934")""")
+db.commit()
+
+cursor.execute("""INSERT INTO Books(title, author, datepublished)
+VALUES("Perfect", "Cecelia Ahern", "2017")""")
+db.commit()
+
+cursor.execute("""INSERT INTO Books(title, author, datepublished)
+VALUES("The marble collector", "Cecelia Ahern", "2016")""")
+db.commit()
+
+
+cursor.execute("""INSERT INTO Books(title, author, datepublished)
+VALUES("The murder on the links", "Agatha Christie", "1923")""")
+db.commit()
+
+cursor.execute("""INSERT INTO Books(title, author, datepublished)
+VALUES("The picture of Dorian Gray", "Oscar Wilde", "1890")""")
+db.commit()
+
+cursor.execute("""INSERT INTO Books(title, author, datepublished)
+VALUES("The secret adversary", "Agatha Christie", "1921")""")
+db.commit()
+
+cursor.execute("""INSERT INTO Books(title, author, datepublished)
+VALUES("The seven dials mystery", "Agatha Christie", "1929")""")
+db.commit()
+
+cursor.execute("""INSERT INTO Books(title, author, datepublished)
+VALUES("The year I met you", "Cecelia Ahern", "2014")""")
+db.commit()
 
 cursor.execute("SELECT * FROM Books")
 for x in cursor.fetchall():
