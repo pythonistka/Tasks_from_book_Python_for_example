95893aff78878de894a5187a3309faf620af5533
Lopatina Darya <baboleh@rambler.ru>
2022-06-18 00:16:13 +0300

    Прописана функция очищения окон в задаче 145 (SqLite)

--- 145.py
+++ 145.py
@@ -1,14 +1,15 @@
 from tkinter import *
 import sqlite3
 
+# def add_name():
 
 
 
-
-
-
-
-
+def clear_window():
+    enter_name.delete(0, END)
+    enter_name.focus()
+    enter_score.delete(0, END)
+    enter_score.focus()
 
 
 window = Tk()
@@ -23,18 +24,18 @@ enter_name.place(x=150, y=20, width=150, height=25)
 enter_name["justify"] = "center"
 enter_name.focus()
 
-enter_age_lbl = Label(text="Введите баллы: ")
-enter_age_lbl.place(x=50, y=50, width=100, height=25)
+enter_score_lbl = Label(text="Введите баллы: ")
+enter_score_lbl.place(x=50, y=50, width=100, height=25)
 
-enter_age = Entry(text="")
-enter_age.place(x=150, y=50, width=150, height=25)
-enter_age["justify"] = "center"
-enter_age.focus()
+enter_score = Entry(text="")
+enter_score.place(x=150, y=50, width=150, height=25)
+enter_score["justify"] = "center"
+enter_score.focus()
 
 add_button = Button(text="Добавить", )
 add_button.place(x=150, y=80, width=70, height=25)
 
-show_button = Button(text="Очистить", )
+show_button = Button(text="Очистить", command=clear_window)
 show_button.place(x=230, y=80, width=70, height=25)
 
 
