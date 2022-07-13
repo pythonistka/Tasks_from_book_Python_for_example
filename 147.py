236d40e1fb7b6cdcce9f9d94b5a6b048a2e4b5fa
Lopatina Darya <baboleh@rambler.ru>
2022-07-14 00:00:55 +0300

    Создано меню программы

---
+++ 147.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+def main():
+    color_list = [1, 2, 3, 4, 5, 6]
+    run = True
+    while run:
+        print("\n" * 1)
+        print("Из заданного списка цветов, выбери 4:")
+        print("[1] ")
+        print("[2] Оранжевый")
+        print("[3] Желтый")
+        print("[4] Зеленый")
+        print("[5] Голубой")
+        print("[6] Фиолетовый")
+        select = []
+        select = int(input("Введи поочередно, через запятую выбранные цвета: "))
+        print("\n")
+        print(select)
