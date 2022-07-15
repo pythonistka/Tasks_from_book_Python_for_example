c45c188960e079298b94689c5080482053195c56
Lopatina Darya <baboleh@rambler.ru>
2022-07-16 01:31:12 +0300

    Создана функция с правилами игры и отредактировано меню

--- 147.py
+++ 147.py
@@ -1,15 +1,7 @@
+import random
 
 
-
-
-
-
-
-def main():
-    color_list = [1, 2, 3, 4, 5, 6]
-    run = True
-    while run:
-        print("\n" * 1)
+def start():
         print("Из заданного списка цветов, выбери 4:")
         print("[1] Красный")
         print("[2] Оранжевый")
@@ -17,7 +9,49 @@ def main():
         print("[4] Зеленый")
         print("[5] Голубой")
         print("[6] Фиолетовый")
-        select = []
-        select = int(input("Введи поочередно, через запятую выбранные цвета: "))
+        select_nums = []
+        select_nums = input("Введи поочередно, через запятую выбранные цвета: ")
         print("\n")
-        print(select)
+        print(select_nums)
+
+        color_list = [1, 2, 3, 4, 5, 6]
+        four_color_from_list = random.choices(color_list, k=4)
+        print(four_color_from_list)
+
+
+pass
+
+
+def rule():
+    print("Компьютер автоматически генерирует комбинацию из четырех цветов из списка возможных.""\n"
+          "Вы вводите комбинацию из четырех цветов из списка, использованного компьютером.""\n"
+          "Программа сообщает, сколько цветов находятся в правильной позиции и сколько правильно угаданных цветов "
+          "находятся в неправильных позициях.""\n"
+          "Вы продолжаете гадать, пока не введете все четыре цвета в правильном порядке"
+          )
+    pass
+
+
+def main():
+    x = 0
+    while x == 0:
+        print("\n")
+        print("1) Правила")
+        print("2) Играть")
+        print("3) Уйти")
+        select = int(input("Выберите номер команды: "))
+        print("\n")
+        if select == 1:
+            rule()
+            x = 0
+        elif select == 2:
+            start()
+            x = x + 1
+        elif select == 3:
+            break
+        else:
+            print("Вы ввели неверную команду")
+            x = 0
+
+
+main()
