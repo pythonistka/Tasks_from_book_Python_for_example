304ab5730be1cb153a3815872d6a245052bcc8f3
Lopatina Darya <baboleh@rambler.ru>
2022-07-29 02:16:10 +0300

    Добавлена функция по поиску имени и замене пароля

--- 148.py
+++ 148.py
@@ -27,7 +27,7 @@ def create_user_id(tmp):
 
 
 def create_password():
-    symbol_list = ["!", "£", "$", "%", "^", "&", "*", "(",")", "?", "@", "#"]
+    symbol_list = ["!", "£", "$", "%", "^", "&", "*", "(", ")", "?", "@", "#"]
     num_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
     try_again = True
     while try_again:
@@ -63,9 +63,8 @@ def create_password():
             print("Умеренно надежный пароль")
             again = input("Попробуешь усложнить пароль?(да/нет): ")
             again.lower()
-            if again != "нет":
-                continue
-            try_again = False
+            if again == "нет":
+                try_again = False
         password_repeat = input("Повторите пароль: ")
         if password_repeat != password:
             print("Пароли не совпадают")
@@ -74,27 +73,38 @@ def create_password():
             return password
 
 
-def change():
-    file = open("Users.csv", "r")
-    change_name = input("Введите имя пользователя для изменения пароля: ")
-    reader = csv.reader(file)
-    for row in file:
-        if change_name in str(row):
-            file = list(csv.reader(open("Users.csv")))
-            tmp = []
-            for rows in file:
-                tmp.append(rows)
-            change_password = input("Введите новый пароль: ")
-            ID = change_name.index(change_name)
-            tmp[ID][1] = change_password
-            print(tmp[ID][1])
-            file = open("Users.csv", "w")
-            x = 0
-            for step in tmp:
-                new_record = tmp[x][0] + ", " + tmp[x][1] + "\n"
-                file.write(new_record)
-                x = x + 1
-            file.close()
+def search_name(tmp):
+    ask_name = True
+    user_id = ""
+    while ask_name == True:
+        search_id = input("Введите имя для поиска: ")
+        search_id.lower()
+        inlist = False
+        row = 0
+        for y in tmp:
+            if search_id in tmp[row][0]:
+                inlist = True
+            row = row + 1
+        if inlist == True:
+            user_id = search_id
+            ask_name = False
+        else:
+            print("Такого имени нет в списке")
+    return user_id
+
+
+def change(user_id, tmp):
+    if user_id != "":
+        password = create_password()
+        id = user_id.index(user_id)
+        tmp[id][1] = password
+        file = open("Users.csv", "w")
+        x = 0
+        for row in tmp:
+            newrec = tmp[x][0] + ", " + tmp[x][1] + "/n"
+            file.write(newrec)
+            x = x + 1
+        file.close()
 
 
 def display(tmp):
@@ -121,7 +131,8 @@ def main():
             file.write(str(new_record))
             file.close()
         elif select == 2:
-            change()
+            user_id = search_name(tmp)
+            change(user_id, tmp)
         elif select == 3:
             display(tmp)
         elif select == 4:
